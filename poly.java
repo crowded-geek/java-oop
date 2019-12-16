1dad2019a117b6c543b61b7615bced4481fac793
crowded <aayushman@aayushman.me>
2019-12-16 15:05:16 +0530

    poly changed

--- poly.java
+++ poly.java
@@ -1,16 +1,26 @@
-class poly {
-    String a = "Hello";
-
-    public static void main(String args[]){
-        inherited in = new inherited();
-        in.main(new String[1]);
+public class poly {
+    public static void main(String args[]) {
+        Vehical a = new Vehical(); // Vehical reference and object
+        Vehical b = new Bike(); // Vehical reference but Bike object
+        Vehical c = new Car(); // Vehical reference but Car object
+        a.move(); // runs the method in Vehical class
+        b.move(); // runs the method in Bike class
+        c.move(); // runs the methon in Car class
     }
 }
-class inherited extends poly {
-    String a = "XD XD";
 
-    public static void main(String args[]) {
-        poly i = new inherited();
-        System.out.println(i.a);
+class Vehical {
+    public void move() {
+        System.out.println("Vehicals can move");
+    }
+}
+class Bike extends Vehical {
+    public void move() {
+        System.out.println("Bike has 2 wheels");
+    }
+}
+class Car extends Vehical {
+    public void move() {
+        System.out.println("Car has 4 wheels");
     }
 }
